d0c7178966524c11e0b1c7e4cbb26eed7fbc8352
unknown <veredpaslev@gmail.com>
2022-03-07 12:45:05 +0200

    updated

--- MyName.java
+++ MyName.java
@@ -1,7 +1,7 @@
 public class MyName{
 	public static void main(String[] args){
 		for(int i=1; i<=10; i++){
-		System.out.println("Vered Paslev with change..."+i);
+		System.out.println("Vered Paslev..."+i);
 		}
 	}
 }
